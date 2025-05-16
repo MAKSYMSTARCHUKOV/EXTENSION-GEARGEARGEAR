77b1bbee3d78e8a8d0eccdbb1fb3c8e99c117153
MAKSYMSTARHUKOV <kiioshi.akayo@gmail.com>
2025-05-16 22:39:27 +0300

    Storage saves state

--- js/background.js
+++ js/background.js
@@ -1,19 +1,16 @@
-
-async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
-}
-
-chrome.action.onClicked.addListener(async () => {
-    const tab = await getCurrentTab()
-    let func = function(){
+const STORAGE =  Object.freeze({
+  'https://uaserial.top': [
+    () => {
       const removeSelectors = [
-        'body > header'
+        'body > header', '.block.movie .left.flex.column'
       ]
       for(const sel of removeSelectors){
         const els = document.querySelectorAll(sel);
         [...els].forEach(el => el?.remove())
+      }
+      const videoContainerHolder = document.querySelector('.block.movie .right.flex.column');
+      if(videoContainerHolder){
+        videoContainerHolder.style.flexGrow = '1';
       }
       const videoHolder = document.querySelector('.player.embed .video-holder.flex.stretch');
       if(videoHolder){
@@ -26,19 +23,53 @@ chrome.action.onClicked.addListener(async () => {
       if(playok){
         playok.style.marginRight = '0';
       }
-    };
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      func
-    });
+    }
+  ]
+})
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+async function executeUrl(tab, origin){
+  try {
+       for(const func of (STORAGE[origin] || [])){
+        chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        func
+      });
+       }
+    } catch (error) {
+      console.error(error?.response?.data?.message || error);
+    }
+}
+
+chrome.action.onClicked.addListener(async () => {
+    const tab = await getCurrentTab()
+    const origin = new URL(tab.url).origin
+    const storage = (await chrome.storage.sync.get()) || {}
+    const state = storage[origin]
+    if(state){
+      storage[origin] = 0
+      chrome.storage.sync.set(storage)
+      chrome.tabs.reload()
+      return
+    }
+    executeUrl(tab, origin)
+    storage[origin] = 1
+    chrome.storage.sync.set(storage)
   })
 
-// chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-//   if(['loading', 'complete'].includes(changeInfo.status)){
-    
-//     chrome.scripting.executeScript({
-//       target: {tabId: tab.id},
-//       func
-//     });
-//   }
-// })
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if(['loading', 'complete'].includes(changeInfo.status)){
+    const tab = await getCurrentTab()
+    const origin = new URL(tab.url).origin
+    const storage = await chrome.storage.sync.get()
+    const state = storage[origin]
+    if(state){
+      executeUrl(tab, origin)
+    }
+  }
+})
